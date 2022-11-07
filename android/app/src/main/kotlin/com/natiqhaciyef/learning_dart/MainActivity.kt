d6154475ee674652a50f824c5d636b7ab37d0d4a
Natiq <natiq00h2272@gmail.com>
2022-11-07 15:10:49 +0400

    Data types

---
+++ android/app/src/main/kotlin/com/natiqhaciyef/learning_dart/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.natiqhaciyef.learning_dart
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/LearningTest.dart
@@ -0,0 +1,16 @@
+void main(){
+  print("Hello world");
+
+  var s = "Natiq" ;    // String
+  var d = 34.2 ;        // Double
+  var i = 181 ;         // Integer
+  var b = true ;        // Boolean
+
+  String name = "Natiq";
+  double height = 1.81 ;
+  int age = 19;
+  bool isStudent = true;
+
+  print("Name: $name, Age: $age, Height: $height, is Student: $isStudent");
+
+}
